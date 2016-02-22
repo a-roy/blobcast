014003dab52c2de5ab2700ab08c67764e78cf37f
Aneesh Roy <aroy@tcd.ie>
2016-02-22 14:14:00 +0000

    Soft body renders using vertex indices

--- SoftBody.h
+++ SoftBody.h
@@ -19,10 +19,12 @@ class SoftBody
 
 		GLuint vao;
 		GLuint *VBOs;
+		GLuint IBO;
 
 		btSoftBody* softbody;
 
 		std::vector<glm::vec3> vertices;
+		std::vector<unsigned int> indices;
 
 		SoftBody(btSoftBody* p_softBody)
 		{
@@ -31,41 +33,49 @@ class SoftBody
 			for (int i = 0; i<softbody->m_faces.size(); ++i)
 			{
 				const btSoftBody::Face&	f = softbody->m_faces[i];
-				vertices.push_back(convert(&f.m_n[0]->m_x));
-				vertices.push_back(convert(&f.m_n[1]->m_x));
-				vertices.push_back(convert(&f.m_n[2]->m_x));
+				const btSoftBody::Node *node = &softbody->m_nodes[0];
+				indices.push_back(f.m_n[0] - node);
+				indices.push_back(f.m_n[1] - node);
+				indices.push_back(f.m_n[2] - node);
+			}
+			for (int i = 0, n = softbody->m_nodes.size(); i < n; i++)
+			{
+				vertices.push_back(convert(&softbody->m_nodes[i].m_x));
 			}
 
 			glGenVertexArrays(1, &vao);
 			VBOs = new GLuint[1];
 			glGenBuffers(1, VBOs);
+			glGenBuffers(1, &IBO);
 
 			glBindVertexArray(vao);
 
 			glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STREAM_DRAW);
 
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 			glBindVertexArray(0);
 		}
 
 		~SoftBody()
 		{
 			delete softbody;
+			delete[] VBOs;
 		}
 
 		void Update()
 		{
 			vertices.clear();
-			for (int i = 0; i<softbody->m_faces.size(); ++i)
+			for (int i = 0, n = softbody->m_nodes.size(); i < n; i++)
 			{
-				const btSoftBody::Face&	f = softbody->m_faces[i];
-				vertices.push_back(convert(&f.m_n[0]->m_x));
-				vertices.push_back(convert(&f.m_n[1]->m_x));
-				vertices.push_back(convert(&f.m_n[2]->m_x));
+				vertices.push_back(convert(&softbody->m_nodes[i].m_x));
 			}
 
 			glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
@@ -78,7 +88,9 @@ class SoftBody
 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 			glBindVertexArray(vao);
-			glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+			glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, (void *)0);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 			glBindVertexArray(0);
 
 			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
