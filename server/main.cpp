ae48170c0f1c591ec5061d90e59b30e1b5638147
Barbara De Kegel <dekegelb@ITSERV.SCSS.TCD.IE>
2016-03-14 18:16:26 +0000

    update quad debug draw

--- server/main.cpp
+++ server/main.cpp
@@ -636,11 +636,11 @@ void draw()
 	blurPass();
 
 	// For debugging
-	//quadShaderProgram->Install();
 	//glActiveTexture(GL_TEXTURE0);
 	//glBindTexture(GL_TEXTURE_2D, gNormal);
-	//quad->Draw();
-	//quadShaderProgram->Uninstall();
+	//quadShaderProgram->Use([&]() {
+	//	quad->Draw();
+	//});
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glActiveTexture(GL_TEXTURE0);
